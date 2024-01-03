da7c9b41350dda54495d963a20ea7bb96a27c0e0
Joaquin C <128087254+Jcamaran@users.noreply.github.com>
2024-01-02 19:29:15 -0500

    Create Can_place_flowers.py

---
+++ EasyProblems/Can_place_flowers.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def canPlaceFlowers(self, flowerbed, n):
+        s = len(flowerbed)
+        bed = [0] + flowerbed + [0]
+        
+        for i in range(1, s+1):
+            if bed[i] == bed[i-1] == bed[i+1] == 0:
+                bed[i] = 1
+                n -= 1
+            
+            if n <= 0: return True
+        
+        return False
