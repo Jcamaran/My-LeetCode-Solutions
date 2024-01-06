394a3fdb84db41c030d57ff23a347489af802b64
Joaquin C <128087254+Jcamaran@users.noreply.github.com>
2024-01-05 23:34:22 -0500

    Create Greatest Common Divisor of strings.py

---
+++ EasyProblems/Greatest Common Divisor of strings.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def gcdOfStrings(self, str1, str2):
+        len1,len2 = len(str1), len(str2)
+        
+        def Divisor(l):
+            if len1 % l or len2 % l:
+                return False
+                f1,f2 = len1 // l, len2 // l 
+                return str1[:l] * f1 == str1 and str1[:l] * f2 == str2
+        
+        for l in range(min(len1,len2), 0 , -1):
+            if Divisor(l):
+                return str1[:l]
+        return ""
+        
+
+
+                
