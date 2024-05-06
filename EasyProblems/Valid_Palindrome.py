fdd162d82c2aa469690d399182a2e5ff348331ca
Joaquin C <128087254+Jcamaran@users.noreply.github.com>
2024-05-05 22:52:21 -0400

    Create Valid_Palindrome.py

---
+++ EasyProblems/Valid_Palindrome.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        l,r = 0,len(s)-1
+        while l < r:
+            while  l < r and not self.alphaNum(s[l]):
+                l += 1
+            while  r > l and not self.alphaNum(s[r]):
+                r -= 1
+            
+            if s[l].lower() != s[r].lower():
+                return False
+            l,r = l + 1, r -1
+        return True 
+
+    def alphaNum(self,c):
+        return (ord('A') <= ord(c) <= ord('Z') or
+        ord('a') <= ord(c) <= ord('z') or
+        ord('0') <= ord(c) <= ord('9'))
+
+
+
