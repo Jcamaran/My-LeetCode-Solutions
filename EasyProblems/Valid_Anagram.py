d41438f57f675af1b1c80b8747634ac494af0e35
Joaquin C <128087254+Jcamaran@users.noreply.github.com>
2024-05-22 11:48:47 -0400

    Update Valid_Anagram.py

--- EasyProblems/Valid_Anagram.py
+++ EasyProblems/Valid_Anagram.py
@@ -1,12 +1,9 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        if len(s) != len(t):
-            return False
-        j = sorted(s)
-        c = sorted(t)
-        if c != j:
-            return False
-        else:
-            return True
-            
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # we know sorted function alphabetically orders lettrers as well as numbers, so we can just remove the first check which makes it run slower
+        s = sorted(s)
+        t = sorted(t)
+        
+        return s == t
 
+        
